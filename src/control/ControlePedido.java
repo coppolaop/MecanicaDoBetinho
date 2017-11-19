20de9dd97e40ce4b5c97cdc5e2bf326874141f43
Marcos A. Gonçalves Junior <mag.junior@hotmail.com>
2017-11-19 17:13:58 -0200

    Funcionalidade de Aceitar Serviço para Cliente
    Um cliente agora pode aceitar um serviço proposto pela oficina

--- src/control/ControlePedido.java
+++ src/control/ControlePedido.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 import java.text.NumberFormat;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -37,16 +38,18 @@ public class ControlePedido extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String cmd = request.getParameter("cmd");
-		if(cmd.equalsIgnoreCase("aprovar")){
+		if(cmd.equalsIgnoreCase("pedidosAtivos")){
+			pedidosAtivos(request,response);
+		}else if(cmd.equalsIgnoreCase("aprovar")){
 			aprovar(request,response);
 		}else if(cmd.equalsIgnoreCase("listar")){
 			listar(request,response);
-		}else if(cmd.equalsIgnoreCase("autorizacao")){
-			pedidosAtivos(request,response);
 		}else if(cmd.equalsIgnoreCase("informacoes")){
 			informacoes(request,response);
 		}else if(cmd.equalsIgnoreCase("listarPecas")){
 			listarPecas(request,response);
+		}else if(cmd.equalsIgnoreCase("state")){
+			state(request,response);
 		}
 	}
 
@@ -55,16 +58,18 @@ public class ControlePedido extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String cmd = request.getParameter("cmd");
-		if(cmd.equalsIgnoreCase("aprovar")){
+		if(cmd.equalsIgnoreCase("pedidosAtivos")){
+			pedidosAtivos(request,response);
+		}else if(cmd.equalsIgnoreCase("aprovar")){
 			aprovar(request,response);
 		}else if(cmd.equalsIgnoreCase("listar")){
 			listar(request,response);
-		}else if(cmd.equalsIgnoreCase("autorizacao")){
-			pedidosAtivos(request,response);
 		}else if(cmd.equalsIgnoreCase("informacoes")){
 			informacoes(request,response);
 		}else if(cmd.equalsIgnoreCase("listarPecas")){
 			listarPecas(request,response);
+		}else if(cmd.equalsIgnoreCase("state")){
+			state(request,response);
 		}
 	}
 	
@@ -138,7 +143,7 @@ public class ControlePedido extends HttpServlet {
 			        }
 			        pw.println("<td>");
 			        pw.println("<div class=\"btn-group\">");
-			        pw.println("<a class=\"btn btn-info\" href=\"./ControleItemServico?cmd=listar&id=" + o.getIdOrdemDeServico() + "\"><i class=\"icon_info_alt\"></i></a>");
+			        pw.println("<a class=\"btn btn-info\" href=\"./ControlePedido?cmd=aprovar&id=" + o.getIdOrdemDeServico() + "\"><i class=\"icon_info_alt\"></i></a>");
 			        pw.println("</div>");
 			        pw.println("</td>");
 			
@@ -234,7 +239,89 @@ public class ControlePedido extends HttpServlet {
 	}
 	
 	protected void aprovar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		Integer id = Integer.parseInt(request.getParameter("id"));
+		PrintWriter pw = response.getWriter();
+        request.getRequestDispatcher("/cli/base1.html").include(request, response);
+        HttpSession session = (HttpSession) request.getSession(); 
+		Integer idUsuario = Integer.parseInt(session.getAttribute("user").toString());
+
+        pw.println("<section class=\"wrapper\">");
+        pw.println("<div class=\"row\">");
+        pw.println("<div class=\"col-lg-12\">");
+        pw.println("<h3 class=\"page-header\"><i class=\"fa fa-table\"></i> ORDENS DE SERVIÇO</h3>");
+        pw.println("<ol class=\"breadcrumb\">");
+        pw.println("<li><i class=\"fa fa-home\"></i><a href=\"./index.html\">Home</a></li>");
+        pw.println("<li><i class=\"fa fa-th-list\"></i><a href=\"ControlePedido?cmd=pedidosAtivos\">Meu Pedido</a></li>");
+        pw.println("<li><i class=\"fa fa-wrench\"></i>Serviços Executados</li>");
+        pw.println("</ol>");
+        pw.println("</div>");
+        pw.println("</div>");
+        pw.println("<div class=\"row\">");
+        pw.println("<div class=\"col-lg-12\">");
+        pw.println("<section class=\"panel\">");
+        pw.println("<header class=\"panel-heading\">");
+        pw.println("Itens de Serviço");
+        pw.println("</header>");
+        pw.println("<table class=\"table table-striped table-advance table-hover\">");
+        pw.println("<tbody>");
+        pw.println("<tr>");
+        pw.println("<th><i class=\"fa fa-wrench\"></i> Serviço</th>");
+        pw.println("<th><i class=\"icon_calendar\"></i> Data de Adição</th>");
+        pw.println("<th><i class=\"fa fa-cog\" aria-hidden=\"true\"></i> Quantidade de Peças</th>");
+        pw.println("<th><i class=\"fa fa-money\" aria-hidden=\"true\"></i> Valor Atual</th>");
+        pw.println("<th><i class=\"icon_profile\"></i> Mecânico Responsável</th>");
+        pw.println("<th><i class=\"icon_cogs\"></i> Ação</th>");
+        pw.println("</tr>");
+
+        try {
+        	GenericDao<OrdemDeServico> od = new GenericDao<OrdemDeServico>();
+        	OrdemDeServico o = od.findById(id,OrdemDeServico.class);
+        	List<ItemServico> lista = o.getItensServico();
+        	
+        	if(!o.getVeiculo().getCliente().getIdUsuario().equals(idUsuario)){
+        		throw new Exception("Não permitido acesso a informações de outro Usuário");
+        	}
+        	
+        	ControleOrdem.calculaValor();
+        	
+	        for(ItemServico i : lista){
+	        
+	        	if(!(i == null)){
+	        	
+			        pw.println("<tr>");
+			        pw.println("<td>"+i.getServico().getNome()+"</td>");
+			        pw.println("<td>"+i.getDataAdicao()+"</td>");
+			        pw.println("<td>"+i.getPecas().size()+"</td>");
+			        NumberFormat nf = NumberFormat.getInstance();
+			        nf.setMaximumFractionDigits(2);
+			        nf.setMinimumFractionDigits(2);
+			        pw.println("<td>R$ "+nf.format(i.getValor())+"</td>");
+			        pw.println("<td>"+i.getMecanico().getNome()+"</td>");
+			        pw.println("<td>");
+			        pw.println("<div class=\"btn-group\">");
+			        if(!i.getAutorizacao()){
+			        	pw.println("<a class=\"btn btn-success\" href=\"./ControlePedido?cmd=state&id=" + i.getIdItemServico() + "\"><i class=\"icon_circle-empty\"></i></a>");
+			        }else{
+			        	pw.println("<a class=\"btn btn-success disabled\"><i class=\"icon_check_alt2\"></i></a>");
+			        }
+			        pw.println("</div>");
+			        pw.println("</td>");
+			
+			        pw.println("</tr>");
+	        	}
+	        }
+        } catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+        pw.println("</tbody>");
+        pw.println("</table>");
+        pw.println("</section>");
+        pw.println("</div>");
+        pw.println("</div>");
+        pw.println("</section>");
+
+        request.getRequestDispatcher("/cli/base2.html").include(request, response);
 	}
 	
 	protected void informacoes(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -384,5 +471,45 @@ public class ControlePedido extends HttpServlet {
         pw.println("</section>");
 
         request.getRequestDispatcher("/cli/base2.html").include(request, response);
+	}
+	
+	protected void state(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String resposta = "";
+		PrintWriter out = response.getWriter();
+		Integer idOrdem = 0;
+		try {	
+			Integer id = Integer.parseInt(request.getParameter("id"));
+			GenericDao<ItemServico> isd = new GenericDao<ItemServico>();
+			ItemServico is = isd.findById(id, ItemServico.class);
+			ItemServico ids = (ItemServico) is.clone();
+			idOrdem = is.getOrdemDeServico().getIdOrdemDeServico();
+			
+			if(is.getOrdemDeServico().getStatus().equalsIgnoreCase("ativo")){
+				if(is.getAutorizacao()==false){
+					ids.changeStatus();
+					isd.update(ids);
+				}else{
+					throw new Exception("Um clienten não pode desautorizar um Serviço já aceito, entre em contato com a oficina");
+				}
+			}else{
+				throw new Exception("Não é possível (des)autorizar um Serviço de uma ordem já finalizada");
+			}
+			
+			resposta = "Autorização Alterada";
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			resposta = ex.getMessage();
+		}finally{
+			response.setContentType("text/html");
+	        RequestDispatcher rd = null;
+	        out.println(resposta);
+	        if(idOrdem.equals(0)){
+	        	rd = request.getRequestDispatcher("./ControlePedido?cmd=listar");
+	        }else{
+	        	rd = request.getRequestDispatcher("./ControlePedido?cmd=aprovar&id="+idOrdem);
+	        }
+	       	rd.include(request, response);
+			out.close();
+		}
 	}
 }
